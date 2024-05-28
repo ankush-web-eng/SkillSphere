75b31b9d19439c13015e7b3495596f2822326f6e
Ankush <deshwalankush23@gmail.com>
2024-05-28 21:55:36 +0530

    Transition Added with Framer Motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Animation from "@/components/center/animation";
 import Bottom from "@/components/center/bottom";
@@ -7,39 +7,108 @@ import Primary from "@/components/center/primary";
 import Footer from "@/components/layouts/footer";
 import Hero from "@/components/layouts/hero";
 import Navbar from "@/components/layouts/navbar";
-import Secondary  from "@/components/layouts/secondary";
+import Secondary from "@/components/layouts/secondary";
 import Tertiary from "@/components/layouts/tertiary";
 import { Button } from "@/components/ui/button";
 
+import PageWrapper from "./page-wrapper";
+
+import { motion, useScroll } from "framer-motion";
+
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+
   return (
-    <div className="flex flex-col min-w-screen">
-      <Navbar />
-      <Hero />
+    <PageWrapper>
+      <div className="flex flex-col min-w-screen">
+        <Navbar />
+        <Hero />
 
-      <div className="py-6 text-2xl md:text-4xl flex justify-center max-md:px-2 text-gray-600 flex-col items-center">
-        <h1>Unlock The Power Of</h1>
-        <h1>Collaboration: Skill Sphere</h1>
-        <p className="text-sm text-center text-gray-400 pt-4">Join our network of talented freelancers and unlock new opportunities</p>
+        <div className="py-6 text-2xl md:text-4xl flex justify-center max-md:px-2 text-gray-600 flex-col items-center">
+          <h1>Unlock The Power Of</h1>
+          <h1>Collaboration: Skill Sphere</h1>
+          <p className="text-sm text-center text-gray-400 pt-4">
+            Join our network of talented freelancers and unlock new
+            opportunities
+          </p>
+        </div>
+
+        <motion.div 
+          // style={{ scaleX: scrollYProgress }}
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Secondary />
+        </motion.div>
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Tertiary />
+        </motion.div>
+        
+
+        <motion.div
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}} 
+          className="py-8 flex justify-center">
+          <div className="w-1/2 flex justify-end">
+            <Button variant={"default"} className="px-10 text-xl rounded-full">
+              Get In Touch
+            </Button>
+          </div>
+        </motion.div>
+
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Primary />
+        </motion.div>
+
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Floats />
+        </motion.div>
+
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Animation />
+        </motion.div>
+
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Bottom />
+        </motion.div>
+        
+
+        <motion.div
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          className="pt-3 pb-10"></motion.div>
+
+        <motion.div 
+          initial={{opacity:0}}
+          whileInView={{opacity:1}}
+          transition={{delay: 0.3, duration: 0.6, ease: 'easeInOut'}}
+          >
+          <Footer />
+        </motion.div>
       </div>
-
-      <Secondary />
-      <Tertiary />
-
-      <div className="py-8 flex justify-center">
-      <div className="w-1/2 flex justify-end">
-        <Button variant={'default'} className="px-10 text-xl rounded-full">Get In Touch</Button>
-      </div>
-      </div>
-
-      <Primary />
-      <Floats />
-      <Animation />
-      <Bottom />
-
-      <div className="pt-3 pb-10"></div>
-
-      <Footer />
-    </div>
-  )
+    </PageWrapper>
+  );
 }
--- components/center/floats.tsx
+++ components/center/floats.tsx
@@ -1,12 +1,18 @@
+import { motion } from "framer-motion";
+
 export default function Floats() {
   return (
     <div className="md:h-screen py-12 h-auto flex md:flex-row flex-col">
-
-      <div className="flex space-y-6 md:w-1/3 w-full items-center flex-col md:h-full">
+      <motion.div
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        exit={{opacity:1}}
+        transition={{delay:0.3,duration:0.6,ease:'easeInOut'}}
+        className="flex space-y-6 md:w-1/3 w-full items-center flex-col md:h-full"
+      >
         <div className="bg-gray-500 md:w-5/6 md:h-1/2 w-80 h-60"></div>
         <div className="bg-gray-500 md:w-3/4 md:h-1/2 w-60 h-80"></div>
-      </div>
-
+      </motion.div>
 
       <div className="flex flex-col items-center justify-around md:h-full md:w-1/3 space-y-10 max-md:py-12">
         <div className="text-gray-400 flex-col flex">
@@ -18,14 +24,25 @@ export default function Floats() {
             auwiefjuwae fjwafwauiefj ijeawfoiweaīf awefwke
           </p>
         </div>
-        <div className="bg-gray-500 md:w-5/6 md:h-1/2 h-60 w-60 max-md:py-20"></div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 1 }}
+          transition={{ delay: 0.3, duration: 0.6, ease: "easeInOut" }}
+          className="bg-gray-500 md:w-5/6 md:h-1/2 h-60 w-60 max-md:py-20"
+        ></motion.div>
       </div>
 
-      
-      <div className="flex space-y-6 md:w-1/3 w-full items-center flex-col md:h-full">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.6, ease: "easeInOut" }}
+        className="flex space-y-6 md:w-1/3 w-full items-center flex-col md:h-full"
+      >
         <div className="bg-gray-500 md:w-3/4 md:h-1/2 w-60 h-80"></div>
         <div className="bg-gray-500 md:w-5/6 md:h-1/2 w-80 h-60"></div>
-      </div>
+      </motion.div>
     </div>
   );
 }
